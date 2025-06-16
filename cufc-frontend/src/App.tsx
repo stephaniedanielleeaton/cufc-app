b69349af97dd53a19317dacdba812f0b07c3657a
Stephanie <stephaniedanielleeaton@gmail.com>
2025-06-15 20:29:54 -0400

    Replaced front end with the create app vite rather than the old CRA script, which relied on react-script which is a library that is no longer maintained.

--- cufc-frontend/src/App.tsx
+++ cufc-frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-import { getConfig, AppConfig } from './utils/config';
+import { getConfig } from './utils/config';
+import type { AppConfig } from './utils/config';
 
 function App() {
   const [backendValue, setBackendValue] = React.useState('');
